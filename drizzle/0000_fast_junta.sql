da5bea734923f6288550f4416d4b4236f55e8631
Alvaro J. Resplandor <alvarojrr79@gmail.com>
2024-02-20 21:28:44 -0400

    fix: add clients and sales

--- drizzle/0000_fast_junta.sql
+++ drizzle/0000_fast_junta.sql
@@ -1,3 +1,14 @@
+CREATE TABLE `clients` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`email` varchar(256) NOT NULL,
+	`phone_number` varchar(20) NOT NULL,
+	`full_name` varchar(256) NOT NULL,
+	`address` varchar(256) NOT NULL,
+	CONSTRAINT `clients_id` PRIMARY KEY(`id`),
+	CONSTRAINT `clients_email_unique` UNIQUE(`email`),
+	CONSTRAINT `clients_phone_number_unique` UNIQUE(`phone_number`)
+);
+--> statement-breakpoint
 CREATE TABLE `employees` (
 	`id` serial AUTO_INCREMENT NOT NULL,
 	`full_name` varchar(256) NOT NULL,
@@ -18,7 +29,7 @@ CREATE TABLE `equipments` (
 --> statement-breakpoint
 CREATE TABLE `messages` (
 	`id` serial AUTO_INCREMENT NOT NULL,
-	`room_id` bigint unsigned NOT NULL,
+	`user_id` bigint unsigned NOT NULL,
 	`created_at` timestamp DEFAULT (now()),
 	`text` text NOT NULL,
 	`type` enum('USER','AI') NOT NULL,
@@ -57,9 +68,18 @@ CREATE TABLE `products_resources` (
 	`id` serial AUTO_INCREMENT NOT NULL,
 	`product_id` bigint unsigned NOT NULL,
 	`resource_id` bigint unsigned NOT NULL,
-	`hours` int unsigned NOT NULL,
+	`quantity` int unsigned NOT NULL,
 	CONSTRAINT `products_resources_id` PRIMARY KEY(`id`),
 	CONSTRAINT `product_resource_unique` UNIQUE(`product_id`,`resource_id`)
+);
+--> statement-breakpoint
+CREATE TABLE `products_sales` (
+	`id` serial AUTO_INCREMENT NOT NULL,
+	`product_id` bigint unsigned NOT NULL,
+	`sales_id` bigint unsigned NOT NULL,
+	`quantity` int unsigned NOT NULL,
+	CONSTRAINT `products_sales_id` PRIMARY KEY(`id`),
+	CONSTRAINT `product_sale_unique` UNIQUE(`product_id`,`sales_id`)
 );
 --> statement-breakpoint
 CREATE TABLE `resources` (
@@ -70,10 +90,12 @@ CREATE TABLE `resources` (
 	CONSTRAINT `resources_id` PRIMARY KEY(`id`)
 );
 --> statement-breakpoint
-CREATE TABLE `rooms` (
+CREATE TABLE `sales` (
 	`id` serial AUTO_INCREMENT NOT NULL,
-	`user_id` bigint unsigned NOT NULL,
-	CONSTRAINT `rooms_id` PRIMARY KEY(`id`)
+	`client_id` bigint unsigned NOT NULL,
+	`date` timestamp DEFAULT (now()),
+	`total` decimal(10,2) NOT NULL,
+	CONSTRAINT `sales_id` PRIMARY KEY(`id`)
 );
 --> statement-breakpoint
 CREATE TABLE `users` (
@@ -86,11 +108,13 @@ CREATE TABLE `users` (
 	CONSTRAINT `users_email_unique` UNIQUE(`email`)
 );
 --> statement-breakpoint
-ALTER TABLE `messages` ADD CONSTRAINT `messages_room_id_rooms_id_fk` FOREIGN KEY (`room_id`) REFERENCES `rooms`(`id`) ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE `messages` ADD CONSTRAINT `messages_user_id_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE `products_employees` ADD CONSTRAINT `products_employees_product_id_products_id_fk` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE `products_employees` ADD CONSTRAINT `products_employees_employee_id_employees_id_fk` FOREIGN KEY (`employee_id`) REFERENCES `employees`(`id`) ON DELETE restrict ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE `products_equipments` ADD CONSTRAINT `products_equipments_product_id_products_id_fk` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE `products_equipments` ADD CONSTRAINT `products_equipments_equipment_id_equipments_id_fk` FOREIGN KEY (`equipment_id`) REFERENCES `equipments`(`id`) ON DELETE restrict ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE `products_resources` ADD CONSTRAINT `products_resources_product_id_products_id_fk` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE cascade ON UPDATE no action;--> statement-breakpoint
 ALTER TABLE `products_resources` ADD CONSTRAINT `products_resources_resource_id_resources_id_fk` FOREIGN KEY (`resource_id`) REFERENCES `resources`(`id`) ON DELETE restrict ON UPDATE no action;--> statement-breakpoint
-ALTER TABLE `rooms` ADD CONSTRAINT `rooms_user_id_users_id_fk` FOREIGN KEY (`user_id`) REFERENCES `users`(`id`) ON DELETE cascade ON UPDATE no action;
+ALTER TABLE `products_sales` ADD CONSTRAINT `products_sales_product_id_products_id_fk` FOREIGN KEY (`product_id`) REFERENCES `products`(`id`) ON DELETE restrict ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE `products_sales` ADD CONSTRAINT `products_sales_sales_id_sales_id_fk` FOREIGN KEY (`sales_id`) REFERENCES `sales`(`id`) ON DELETE restrict ON UPDATE no action;--> statement-breakpoint
+ALTER TABLE `sales` ADD CONSTRAINT `sales_client_id_clients_id_fk` FOREIGN KEY (`client_id`) REFERENCES `clients`(`id`) ON DELETE restrict ON UPDATE no action;
